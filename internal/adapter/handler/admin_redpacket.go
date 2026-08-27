5a01b716596163d615d065a8de8ad13b1b871193
Owen123 <cnwen123@gmail.com>
2026-08-27 23:40:44 +0700

    fix: rename module path in internal/adapter/handler/admin_redpacket.go

--- internal/adapter/handler/admin_redpacket.go
+++ internal/adapter/handler/admin_redpacket.go
@@ -6,9 +6,9 @@ import (
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 
-	"github.com/TGlimmer/TG_walletbot/internal/domain/entity"
-	"github.com/TGlimmer/TG_walletbot/internal/domain/port"
-	"github.com/TGlimmer/TG_walletbot/pkg/response"
+	"github.com/OwenALL/TG_walletbot/internal/domain/entity"
+	"github.com/OwenALL/TG_walletbot/internal/domain/port"
+	"github.com/OwenALL/TG_walletbot/pkg/response"
 )
 
 // AdminRedPacketHandler 红包管理处理器
