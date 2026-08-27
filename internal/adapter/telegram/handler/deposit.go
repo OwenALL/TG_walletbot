fc5c8716415d1b182c2f66d42a38ac9b551ffc36
Owen123 <cnwen123@gmail.com>
2026-08-27 23:41:32 +0700

    fix: rename module path in internal/adapter/telegram/handler/deposit.go

--- internal/adapter/telegram/handler/deposit.go
+++ internal/adapter/telegram/handler/deposit.go
@@ -8,9 +8,9 @@ import (
 	"github.com/go-telegram/bot/models"
 	"go.uber.org/zap"
 
-	"github.com/TGlimmer/TG_walletbot/internal/adapter/telegram"
-	"github.com/TGlimmer/TG_walletbot/internal/app"
-	tgpkg "github.com/TGlimmer/TG_walletbot/pkg/telegram"
+	"github.com/OwenALL/TG_walletbot/internal/adapter/telegram"
+	"github.com/OwenALL/TG_walletbot/internal/app"
+	tgpkg "github.com/OwenALL/TG_walletbot/pkg/telegram"
 )
 
 // DepositHandler 充值相关回调处理器
