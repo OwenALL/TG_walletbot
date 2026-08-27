3cd80ae4a8b3dd494fb6cf9e6a3b6747bbe4c805
Owen123 <cnwen123@gmail.com>
2026-08-27 23:41:14 +0700

    fix: rename module path in internal/adapter/middleware/jwt.go

--- internal/adapter/middleware/jwt.go
+++ internal/adapter/middleware/jwt.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/TGlimmer/TG_walletbot/pkg/response"
+	"github.com/OwenALL/TG_walletbot/pkg/response"
 )
 
 // AdminClaims 管理员 JWT Claims
