f623d36e663704870481600cd6b023701afce6e6
Owen123 <cnwen123@gmail.com>
2026-08-27 23:40:32 +0700

    fix: rename module path in internal/adapter/handler/admin_finance.go

--- internal/adapter/handler/admin_finance.go
+++ internal/adapter/handler/admin_finance.go
@@ -9,9 +9,9 @@ import (
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 
-	"github.com/TGlimmer/TG_walletbot/internal/domain/entity"
-	"github.com/TGlimmer/TG_walletbot/internal/domain/port"
-	"github.com/TGlimmer/TG_walletbot/pkg/response"
+	"github.com/OwenALL/TG_walletbot/internal/domain/entity"
+	"github.com/OwenALL/TG_walletbot/internal/domain/port"
+	"github.com/OwenALL/TG_walletbot/pkg/response"
 )
 
 // AdminFinanceHandler 余额宝管理处理器
